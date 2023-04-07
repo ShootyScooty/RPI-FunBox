8d8d1c0428bf568b4fd3da8f1a3dabf5d2761184
Aidan LeMay <aidanpathways12@gmail.com>
2023-04-07 19:21:00 -0400

    strings

--- weatherForecast.py
+++ weatherForecast.py
@@ -12,18 +12,18 @@ printer = ThermalPrinter(uart)
 current_time = datetime.datetime.now()
 
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
-complete_url = base_url + "appid=" + API_KEY + "&units=imperial&q=14611" 
+complete_url = base_url, "appid=", API_KEY, "&units=imperial&q=14611" 
 response = requests.get(complete_url)
 
 weather = response.json()
 
 if weather["cod"] != "404":
 
-	printer.print("Today is " + current_time.strftime('%A') + ", " + current_time.month + " " + current_time.day + ", " + current_time.year)
+	printer.print("Today is ", current_time.strftime('%A'), ", ", current_time.month, " ", current_time.day, ", ", current_time.year)
 	printer.feed(5)
-	printer.print("Today in " + weather['sys'].name + " there will be a high of " + weather['main'].temp_max + " and a low of " + weather['main'].temp_min + ", but currently it's " + weather['main'].temp + " but feels like " + weather['main'].feels_like)
+	printer.print("Today in ", weather['sys'].name, " there will be a high of ", weather['main'].temp_max, " and a low of ", weather['main'].temp_min, ", but currently it's ", weather['main'].temp, " but feels like ", weather['main'].feels_like)
 	printer.feed(5)
-	printer.print("You can expect " + weather['weather'][0].main + ", specifically " + weather['weather'][0].description)
+	printer.print("You can expect ", weather['weather'][0].main, ", specifically ", weather['weather'][0].description)
 	printer.feed(10)
 
 else:
