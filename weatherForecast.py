1c82b10bab01636fbe582ed53c25b0e8d5a30654
Aidan LeMay <aidanpathways12@gmail.com>
2023-04-07 23:22:39 -0400

    more test

--- weatherForecast.py
+++ weatherForecast.py
@@ -19,11 +19,11 @@ weather = response.json()
 
 if weather["cod"] != "404":
 
-	print(weather)
-
 	line1 = "Today is ", current_time.strftime('%A'), ", ", current_time.month, " ", current_time.day, ", ", current_time.year
 	line2 = "Today in ", weather['name'], " there will be a high of ", weather['main']['temp_max'], " and a low of ", weather['main']['temp_min'], ", but currently it's ", weather['main']['temp'], " but feels like ", weather['main']['feels_like']
 	line3 = "You can expect ", weather['weather'][0]['main'], ", specifically ", weather['weather'][0]['description']
+
+	print(line1, line2, line3)
 
 	printer.print(line1)
 	printer.feed(5)
