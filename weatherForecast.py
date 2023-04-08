fd52bcc997f6ea7063da304e003b5a610413aa8d
Aidan LeMay <aidanpathways12@gmail.com>
2023-04-07 23:58:15 -0400

    changing some files and making the button polling func

--- weatherForecast.py
+++ weatherForecast.py
@@ -5,42 +5,44 @@ import adafruit_thermal_printer
 import datetime
 from keys import *
 
-uart = serial.Serial("/dev/serial0", baudrate=19200, timeout=5000)
-ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.16)
-printer = ThermalPrinter(uart)
+def weatherPrint():
 
-current_time = datetime.datetime.now()
+	uart = serial.Serial("/dev/serial0", baudrate=19200, timeout=5000)
+	ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.16)
+	printer = ThermalPrinter(uart)
 
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-complete_url = base_url + "appid=" + API_KEY + "&units=imperial&q=14611" 
-response = requests.get(complete_url)
+	current_time = datetime.datetime.now()
 
-weather = response.json()
+	base_url = "http://api.openweathermap.org/data/2.5/weather?"
+	complete_url = base_url + "appid=" + API_KEY + "&units=imperial&q=14611" 
+	response = requests.get(complete_url)
 
-if weather["cod"] != "404":
+	weather = response.json()
 
-	line0 = "###############################"
-	line1 = " _ _ _         _   _           "
-	line2 = "| | | |___ ___| |_| |_ ___ ___ "
-	line3 = "| | | | -_| .'|  _|   | -_|  _|"
-	line4 = "|_____|___|__,|_| |_|_|___|_| "
+	if weather["cod"] != "404":
 
-	printer.print(line0)
-	printer.print(line1)
-	printer.print(line2)
-	printer.print(line3)
-	printer.print(line4)
-	printer.print(line0)
+		line0 = "###############################"
+		line1 = " _ _ _         _   _           "
+		line2 = "| | | |___ ___| |_| |_ ___ ___ "
+		line3 = "| | | | -_| .'|  _|   | -_|  _|"
+		line4 = "|_____|___|__,|_| |_|_|___|_| "
 
-	printer.feed(2)
+		printer.print(line0)
+		printer.print(line1)
+		printer.print(line2)
+		printer.print(line3)
+		printer.print(line4)
+		printer.print(line0)
 
-	printer.print("Today is " + current_time.strftime('%A') + ", " + str(current_time.month) + "/" + str(current_time.day) + "/" + str(current_time.year))
-	printer.feed(1)
-	printer.print("Today in " + weather['name'] + "\nthere will be a high of " + str(weather['main']['temp_max']) + "\nand a low of " + str(weather['main']['temp_min']) + "\nbut currently it's " + str(weather['main']['temp']) + "\nbut feels like " + str(weather['main']['feels_like']))
-	printer.feed(1)
-	printer.print("You can expect " + weather['weather'][0]['main'] + ",\nspecifically " + weather['weather'][0]['description'])
-	printer.feed(5)
+		printer.feed(2)
 
-else:
-	printer.print(" City Not Found ")
-	printer.feed(10)
+		printer.print("Today is " + current_time.strftime('%A') + ", " + str(current_time.month) + "/" + str(current_time.day) + "/" + str(current_time.year))
+		printer.feed(1)
+		printer.print("Today in " + weather['name'] + "\nthere will be a high of " + str(weather['main']['temp_max']) + "\nand a low of " + str(weather['main']['temp_min']) + "\nbut currently it's " + str(weather['main']['temp']) + "\nbut feels like " + str(weather['main']['feels_like']))
+		printer.feed(1)
+		printer.print("You can expect " + weather['weather'][0]['main'] + ",\nspecifically " + weather['weather'][0]['description'])
+		printer.feed(5)
+
+	else:
+		printer.print(" City Not Found ")
+		printer.feed(10)
