2db8df650876f89bf33a1e81a2132580640b26c0
Aidan LeMay <aidanpathways12@gmail.com>
2023-04-07 23:18:33 -0400

    variable formatting

--- weatherForecast.py
+++ weatherForecast.py
@@ -20,7 +20,7 @@ weather = response.json()
 if weather["cod"] != "404":
 
 	line1 = "Today is ", current_time.strftime('%A'), ", ", current_time.month, " ", current_time.day, ", ", current_time.year
-	line2 = "Today in ", weather['sys'].name, " there will be a high of ", weather['main'].temp_max, " and a low of ", weather['main'].temp_min, ", but currently it's ", weather['main'].temp, " but feels like ", weather['main'].feels_like
+	line2 = "Today in ", weather.name, " there will be a high of ", weather['main'].temp_max, " and a low of ", weather['main'].temp_min, ", but currently it's ", weather['main'].temp, " but feels like ", weather['main'].feels_like
 	line3 = "You can expect ", weather['weather'][0].main, ", specifically ", weather['weather'][0].description
 
 	printer.print(line1)
