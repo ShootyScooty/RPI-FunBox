d60b1feaa2253f1b31e622f888582659c11b190e
Aidan LeMay <aidanpathways12@gmail.com>
2023-12-13 11:18:51 -0500

    no delim??

--- healthCheck.py
+++ healthCheck.py
@@ -1,8 +1,8 @@
 import subprocess
 
-out = str(subprocess.check_output(['systemctl', '--host', 'aidan@192.168.1.31', 'status', 'MC-Emergency-Bot']))
+out = subprocess.check_output(['systemctl', '--host', 'aidan@192.168.1.31', 'status', 'MC-Emergency-Bot'])
 
-mcBot = out.split("\n")
+mcBot = out.split()
 
 for x in mcBot:
     print(x)
