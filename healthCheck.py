c19f03ff560f83473d7032e4974ddc71b6d0eda9
Aidan LeMay <aidanpathways12@gmail.com>
2023-12-13 11:33:54 -0500

    Using paramiko instead

--- healthCheck.py
+++ healthCheck.py
@@ -1,8 +1,46 @@
-import subprocess
+import paramiko
+import systemd
 
-out = str(subprocess.check_output(['cat', 'botHealth.py', '|', 'ssh', 'aidan@192.168.1.31', 'python', '-']))
+def get_service_status(host, username, service_name):
+    try:
+        # SSH connection setup
+        ssh = paramiko.SSHClient()
+        ssh.load_system_host_keys()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, username=username)
 
-print(out)
+        # Get service status using systemd
+        manager = systemd.manager.Manager()
+        service = manager.get_unit(service_name)
+        status = service.properties["ActiveState"]
+        since = service.properties["ActiveEnterTimestampMonotonic"]
+        uptime = service.properties["ActiveEnterTimestampMonotonic"]
+
+        # Print the results
+        print(f"Service: {service_name}")
+        print(f"Status: {status}")
+        print(f"Since: {since}")
+        print(f"Uptime: {uptime}")
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        # Close the SSH connection
+        if ssh:
+            ssh.close()
+
+if __name__ == "__main__":
+    host = "192.168.1.31"
+    username = "aidan"
+    service_name = "MC-Emergency-Bot.service"
+
+    get_service_status(host, username, service_name)
+
+# import subprocess
+
+# out = str(subprocess.check_output(['cat', 'botHealth.py', '|', 'ssh', 'aidan@192.168.1.31', 'python', '-']))
+
+# print(out)
 
 # mcBot = out.split("\n     ")
 
