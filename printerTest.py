1e9badce22f64b9f39de446d4e633b4a50a7e8dd
Aidan LeMay <aidanpathways12@gmail.com>
2023-12-13 12:41:06 -0500

    test

--- printerTest.py
+++ printerTest.py
@@ -4,4 +4,9 @@ trhost = "192.168.1.61"
 trusername = "aidan"
 trservice_name = "trunk-recorder"
 
+mchost = "192.168.1.31"
+mcusername = "aidan"
+mcservice_name = "MC-Emergency-Bot.service"
+
+print(get_service_status(mchost, mcusername, mcservice_name))
 print(get_service_status(trhost, trusername, trservice_name))
