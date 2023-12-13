88a16ce41912b6273c2b407f45816e0411de610c
Aidan LeMay <aidanpathways12@gmail.com>
2023-12-13 11:14:23 -0500

    Formatting?

--- healthCheck.py
+++ healthCheck.py
@@ -1,3 +1,7 @@
 import subprocess
 
-print(subprocess.check_output(['systemctl', '--host', 'aidan@192.168.1.31', 'status', 'MC-Emergency-Bot']))
+out = subprocess.check_output(['systemctl', '--host', 'aidan@192.168.1.31', 'status', 'MC-Emergency-Bot'])
+
+mcBot = out.split("\n")
+
+print(mcBot)
