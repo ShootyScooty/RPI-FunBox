76e061a5d6a059ad06a6b4c279a300cea43e1504
Aidan LeMay <aidanpathways12@gmail.com>
2023-12-12 22:33:55 -0500

    Updating Button Polling

--- buttonPolling.py
+++ buttonPolling.py
@@ -3,30 +3,52 @@
 # http://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio  
 import RPi.GPIO as GPIO
 from weatherForecast import *
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 # Pin 23: Blue Button
 blue = 23
 green = 24
 yellow = 25
 red = 12
-clear = 16
+white = 16
 
-GPIO.setup(blue, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(green, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(yellow, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(red, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(clear, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(blue, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(green, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(yellow, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(red, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(white, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
 
-print("Polling Started")
+GPIO.add_event_detect(blue, GPIO.BOTH, callback=pushBlue,bouncetime=800)
+GPIO.add_event_detect(green, GPIO.BOTH, callback=pushGreen, bouncetime=800)
+GPIO.add_event_detect(yellow, GPIO.BOTH, callback=pushYellow, bouncetime=800)
+GPIO.add_event_detect(red, GPIO.BOTH, callback=pushRed, bouncetime=800)
+GPIO.add_event_detect(white, GPIO.BOTH, callback=pushWhite, bouncetime=800)
 
-try:  
-    while True:
-        GPIO.wait_for_edge(blue, GPIO.FALLING)
-        print("Running Weather")
-        weatherPrint()
+def pushBlue():
+    print("Blue Pressed")
 
-except KeyboardInterrupt:
-    print("Shutting Down Polling")
-    GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
-GPIO.cleanup()           # clean up GPIO on normal exit  
+def pushGreen():
+    print("Green Pressed")
+
+def pushYellow():
+    print("Yellow Pressed")
+
+def pushRed():
+    print("Red Pressed")
+
+def pushWhite():
+    print("White Pressed")
+
+while True:
+    print("Polling Started")
+
+# try:  
+#     while True:
+#         GPIO.wait_for_edge(blue, GPIO.FALLING)
+#         print("Running Weather")
+#         weatherPrint()
+
+# except KeyboardInterrupt:
+#     print("Shutting Down Polling")
+#     GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
+# GPIO.cleanup()           # clean up GPIO on normal exit  
