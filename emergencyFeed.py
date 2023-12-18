7dc67c778e57cdb713cdb6de368c066b4c34bcd6
Aidan LeMay <aidanpathways12@gmail.com>
2023-12-18 11:18:10 -0500

    Improved emergencyFeed Function

--- emergencyFeed.py
+++ emergencyFeed.py
@@ -1,7 +1,7 @@
 import requests, json
 import serial
 import adafruit_thermal_printer
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 from requests_html import HTMLSession
 from printerTools import *
 
@@ -9,8 +9,12 @@ uart = serial.Serial("/dev/serial0", baudrate=19200, timeout=3000)
 ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.16)
 printer = ThermalPrinter(uart)
 
-monems = "https://clearcutradio.app/api/v1/calls?system=us-ny-monroe&talkgroup=1077"
-henfire = "https://clearcutradio.app/api/v1/calls?system=us-ny-monroe&talkgroup=1654"
+ts_now = int(time.time())
+
+ts_24 = ts_now - (24 * 60 * 60)
+
+monems = "https://clearcutradio.app/api/v1/calls?system=us-ny-monroe&talkgroup=1077" + "&before_ts=" + ts_now + "&after_ts=" + ts_24
+henfire = "https://clearcutradio.app/api/v1/calls?system=us-ny-monroe&talkgroup=1654" + "&before_ts=" + ts_now + "&after_ts=" + ts_24
 
 # ------------------ ClearCut Functions ------------------
 
@@ -27,7 +31,7 @@ def get_source_clearcut(url):
 
 # ------------------ RSS Functions ------------------
 
-def get_source():
+def get_source_rss():
     try:
         session = HTMLSession()
         response = session.get("https://www.monroecounty.gov/incidents911.rss")
@@ -37,9 +41,16 @@ def get_source():
         print(e)    
 
 # ------------------ End RSS Functions ------------------
+        
+def matching(sentence, words):
+    # Convert both the sentence and words to lowercase for case-insensitive comparison
+    lowercase_sentence = sentence.lower()
+    lowercase_words = [word.lower() for word in words]
+
+    # Check if any word in the array is present in the sentence
+    return any(word in lowercase_sentence for word in lowercase_words)
 
 def printEmergency():
-    response = get_source()
 
     printer.print("###############################")
     printer.print(center_text("  ___    __   __ "))
@@ -50,23 +61,11 @@ def printEmergency():
     printer.print(center_text("  /_/    |_|  |_|"))
     printer.print("###############################")
 
-    printer.print("Monroe County 911 Events:")
-    printer.feed(1)
+    events = 0
 
-    with response as r:
-        items = r.html.find("item", first=False)
+    keywords = ["RIT", "rit", "R I T", "r i t", "6359", "6 3 5 9", "6-3-5-9", "Defib 63", "DEFIB 63", "defib 63", "Defib 6-3", "DEFIB 6-3", "defib 6-3", "601", "6 0 1", "Andrews", "Andrews Memorial", "Andrews Memorial Drive", "Lomb", "Lomb Memorial", "Lomb Memorial Drive", "Lowenthall", "Perkins", "Riverknoll", "University Commons", "John", "John Street", "Wiltsie", "Greenleaf", "Greenleaf Court", "Gleason", "Reynolds", "Kimball", "Farnum", "Charters"]
 
-        for item in items:        
-
-            title = item.find('title', first=True).text
-
-            if not title.startswith("PARKING INCIDENT"):
-                description = item.find('description', first=True).text
-
-                pubdate = item.find('pubDate', first=True).text
-
-                printer.print(str(title + " | " + description + " | " + pubdate))
-                printer.feed(1)
+    
 
     response = get_source_clearcut(monems)
 
@@ -78,8 +77,8 @@ def printEmergency():
             timestamp = datetime.fromtimestamp(data['startTime']) - timedelta(hours = 4)
             text = data['transcript']['text']
 
-            # Get all calls within num range with matching keywords
-            if ("RIT" in text or "6359" in text or "6-3-5-9" in text or "Defib 63" in text or "DEFIB 63" in text or "defib 63" in text):
+            if (matching(text, keywords)):
+                events += 1
                 printer.print(str(timestamp) + " | " + text)
                 printer.feed(1)
 
@@ -93,10 +92,31 @@ def printEmergency():
             timestamp = datetime.fromtimestamp(data['startTime']) - timedelta(hours = 4)
             text = data['transcript']['text']
 
-            # Get all calls within num range with matching keywords
-            if ("RIT" in text):
+            if (matching(text, keywords)):
                printer.print(str(timestamp) + " | " + text)
                printer.feed(1)
+
+    if events == 0:
+        response = get_source_rss()
+        printer.print("Monroe County 911 Events:")
+        printer.feed(1)
+
+        with response as r:
+            items = r.html.find("item", first=False)
+
+            for item in items:        
+
+                title = item.find('title', first=True).text
+
+                if not title.startswith("PARKING INCIDENT") and events < 11:
+                    description = item.find('description', first=True).text
+
+                    pubdate = item.find('pubDate', first=True).text
+
+                    printer.print(str(title + " | " + description + " | " + pubdate))
+                    printer.feed(1)
+
+                    events += 1
 
     printer.print("###############################")
     printer.feed(4)
